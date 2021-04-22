a738eb950d184301582ec58b0bd18bd04a29dcc6
bobmagicii <bob@majdak.net>
2021-04-22 19:22:10 +0000

    first pass bringing it up current standards and php 8

--- src/Nether/OneScript/FileFinder.php
+++ src/Nether/OneScript/FileFinder.php
@@ -2,21 +2,20 @@
 
 namespace Nether\OneScript;
 
-use \Nether as Nether;
+use FilterIterator;
+use FilesystemIterator;
+use EmptyIterator;
 
-use \Exception;
-use \FilterIterator;
-use \FilesystemIterator;
-use \EmptyIterator;
-
-class FileFinder extends FilterIterator {
+class FileFinder
+extends FilterIterator {
 /*//
 finds files in the specified directory with the allowed list of file
 extensions. packs a FilesystemIterator into a FilterIterator. jakefolio
 will be unable to resist falling in love with me now.
 //*/
 
-	protected $Exts;
+	protected array
+	$Exts = [];
 	/*//
 	@type array
 	list of the extensions we want to allow, flipped into a hash index.
@@ -26,7 +25,7 @@ will be unable to resist falling in love with me now.
 	////////////////////////////////
 
 	public function
-	__construct($Directory, $Extensions=[]) {
+	__Construct(string $Directory, array $Extensions=[]) {
 	/*//
 	@override
 	//*/
@@ -42,7 +41,7 @@ will be unable to resist falling in love with me now.
 
 		// if given some extensions, flip them to generate a hash lookup
 		// table instead.
-		if(is_array($Extensions))
+		if(count($Extensions))
 		$this->Exts = array_flip($Extensions);
 
 		return;
@@ -52,19 +51,20 @@ will be unable to resist falling in love with me now.
 	////////////////////////////////
 
 	public function
-	Accept() {
+	Accept():
+	bool {
 	/*//
 	@override
 	//*/
 
 		// provide a way for me to reuse this to accept all files. then
 		// i can reuse this is a recursive copy.
-		if($this->Exts === null)
-		return true;
+		if($this->Exts === NULL)
+		return TRUE;
 
 		// else do the test.
 		return array_key_exists(
-			$this->getInnerIterator()->getExtension(),
+			$this->GetInnerIterator()?->GetExtension(),
 			$this->Exts
 		);
 	}
